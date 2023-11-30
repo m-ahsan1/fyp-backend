91196f62a318d1eb0005d7eed384bbce561409e1
Abdullah-Sohail100 <90067650+Abdullah-Sohail100@users.noreply.github.com>
2023-11-30 23:55:41 +0500

    backend apis

--- index.js
+++ index.js
@@ -1,8 +1,84 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator');
+
 const app = express();
+const PORT = 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// MongoDB Schemas
+const UserSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+});
+
+const CarSchema = new mongoose.Schema({
+    make: String,
+    model: String,
+    year: Number,
+});
+
+const User = mongoose.model('User', UserSchema);
+const Car = mongoose.model('Car', CarSchema);
+
+// Middleware
+app.use(bodyParser.json());
+
+// User signup endpoint with validation
+app.post('/signup', [
+    check('username').isLength({ min: 5 }).withMessage('Username must be at least 5 characters long'),
+    check('email').isEmail().withMessage('Enter a valid email address'),
+    check('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    const { username, email, password } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+// User login endpoint
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        res.status(200).json({ message: 'Login successful' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
 
 
-const port = 8000;
-app.listen(port, () => {
- console.log(`Welcome to the mern auth tutorial! Server is running on ${port}`)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });

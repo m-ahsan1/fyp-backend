500fe501d5f5ff44a1a06c7f74735a7b8fcc27ea
Muhammad Ahsan <ahsank0811@gmail.com>
2023-12-02 15:23:27 +0500

    fixed server, added schema and routes for blogs feature

--- index.js
+++ index.js
@@ -1,84 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const { check, validationResult } = require('express-validator');
+const express = require("express");
+const mongoose = require("mongoose");
+const blogsRoutes = require("./routes/blogsRoutes");
+const cors = require("cors");
 
+// Create an Express app
 const app = express();
-const PORT = 3000;
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-// MongoDB Schemas
-const UserSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, res.method);
+  next();
 });
 
-const CarSchema = new mongoose.Schema({
-    make: String,
-    model: String,
-    year: Number,
-});
+// Aik he database use karni, no need to create multiple instead create collections in it
+mongoose
+  .connect("mongodb://127.0.0.1:27017/bidding-bazaar")
+  .then(() => console.log("Database Connected!"));
 
-const User = mongoose.model('User', UserSchema);
-const Car = mongoose.model('Car', CarSchema);
+// create routes in the routes folder, import and add here
+app.use("/api/blogs", blogsRoutes);
 
-// Middleware
-app.use(bodyParser.json());
+// Define the port to listen on
+const PORT = process.env.PORT || 3001;
 
-// User signup endpoint with validation
-app.post('/signup', [
-    check('username').isLength({ min: 5 }).withMessage('Username must be at least 5 characters long'),
-    check('email').isEmail().withMessage('Enter a valid email address'),
-    check('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
-], async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }
-
-    const { username, email, password } = req.body;
-
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ username, email, password: hashedPassword });
-        await newUser.save();
-        res.status(201).json({ message: 'User created successfully' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error' });
-    }
-});
-
-// User login endpoint
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const user = await User.findOne({ username });
-        if (!user) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return res.status(401).json({ message: 'Invalid credentials' });
-        }
-
-        res.status(200).json({ message: 'Login successful' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Server error' });
-    }
-});
-
-
-
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });

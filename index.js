03fd8454de42c6857caff74ba05bfb9c40869f0b
Muhammad Ahsan <ahsank0811@gmail.com>
2023-12-03 23:42:37 +0500

    added stripe backend

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const blogsRoutes = require("./routes/blogsRoutes");
 const listingsRoutes = require("./routes/listingsRoutes");
-const analyticsRoutes = require("./routes/analyticsRoutes")
+const analyticsRoutes = require("./routes/analyticsRoutes");
 const userRoutes = require("./routes/userRoutes");
 const notificationRoutes = require("./routes/notificationRoutes");
 const cors = require("cors");
-
+require("dotenv").config();
+const strip = require("stripe")(process.env.SECRET_KEY);
 // Create an Express app
+
 const app = express();
+
+//stripe
+
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -21,6 +26,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use;
 // Aik he database use karni, no need to create multiple instead create collections in it
 mongoose
   .connect("mongodb://127.0.0.1:27017/bidding-bazaar")
@@ -36,6 +42,14 @@ app.use("/api/listings", listingsRoutes);
 app.use("/api/analytics", analyticsRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/notification", notificationRoutes);
+
+//strip
+
+app.post("/api/payment", async (req, res) => {
+  let status, error;
+  const { token, amount } = req.body;
+  console.log(token);
+});
 
 // Define the port to listen on
 const PORT = process.env.PORT || 3001;
